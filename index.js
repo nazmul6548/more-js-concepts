ca162ea0fe4d103b368b25c1dc9e61f2c6eb581b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-29 08:45:49 +0600

    task

---
+++ index.js
@@ -0,0 +1,16 @@
+// Task - 1
+// 1a) Write a function that displays a message after 5s.
+// ans:
+
+
+const setTime = setTimeout(() => {
+    console.log("hello!");
+    },5000);
+
+// 1b) Write a function called delayedGreeting() that takes two parameters, name and delay time, and logs a greeting message after the given delay time.
+
+// Input:
+// delayGreeting(‘Alice’, 2000)
+
+// Output:
+// Hello, Alice!
